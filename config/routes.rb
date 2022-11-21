c7e5eec0024922d7c2dfa408708eea3749582757
Ray Yu <kaito-akira@hotmail.com>
2022-11-21 15:34:43 -0800

    Test Rspec set up

--- config/routes.rb
+++ config/routes.rb
@@ -20,18 +20,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
-    
-  # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-  # root to: 'products#index'
-  
-  # I've created a gif controller so I have a page I can secure later. 
-  # This is optional (as is the root to: above).
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  # These routes will be for signup. The first renders a form in the browse, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
