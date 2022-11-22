56803d51772ab53d1b1bce5fa1d04327db1988e4
Ray Yu <kaito-akira@hotmail.com>
2022-11-21 19:25:09 -0800

    Refactor

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,65 +1,48 @@
 require 'rails_helper'
-require 'products_controller'
 
 describe Product, type: :model do
 
   before do
-    @productAll = Product.new(:name => "test", :price_cents => 120000, :category_id => 2, :quantity => 134)
-    @productName = Product.new(:price_cents => 120000, :category_id => 2, :quantity => 134)
-    @productQuantity = Product.new(:name => "test", :price_cents => 120000, :category_id => 2)
-    @productPrice = Product.new(:name => "test", :category_id => 2, :quantity => 134)
-    @productCategory = Product.new(:name => "test", :price_cents => 120000, :quantity => 134)
+    @All = Product.new(:name => "test", :price_cents => 120000, :category_id => 2, :quantity => 134)
+    @Name = Product.new(:price_cents => 120000, :category_id => 2, :quantity => 134)
+    @Quantity = Product.new(:name => "test", :price_cents => 120000, :category_id => 2)
+    @Price = Product.new(:name => "test", :category_id => 2, :quantity => 134)
+    @Category = Product.new(:name => "test", :price_cents => 120000, :quantity => 134)
 
-    @productAll.save
-    @productName.save
-    @productCategory.save
-    @productQuantity.save
-    @productPrice.save
+    @All.save
+    @Name.save
+    @Category.save
+    @Quantity.save
+    @Price.save
 
   end
   
   describe 'Validations' do
     it 'should have all fields' do
-      expect(@productAll.name).to be_present
-      expect(@productAll.price_cents).to be_present
-      expect(@productAll.quantity).to be_present
-      expect(@productAll.category_id).to be_present
+      expect(@All.name).to be_present
+      expect(@All.price_cents).to be_present
+      expect(@All.quantity).to be_present
+      expect(@All.category_id).to be_present
     end
 
-    it 'should have all fields but name' do
-      expect(@productName.name).to be_nil
-      expect(@productName.price_cents).to be_present
-      expect(@productName.quantity).to be_present
-      expect(@productName.category_id).to be_present
-      
-      expect(@productName.errors.full_messages).to include("Name can't be blank")
+    it 'should have error message if name is empty' do
+      expect(@Name.name).to be_nil
+      expect(@Name.errors.full_messages).to include("Name can't be blank")
     end
 
-    it 'should have all fields but price_cents' do
-      expect(@productPrice.name).to be_present
-      expect(@productPrice.price_cents).to be_nil
-      expect(@productPrice.quantity).to be_present
-      expect(@productPrice.category_id).to be_present
-
-      expect(@productPrice.errors.full_messages).to include("Price can't be blank")
+    it 'should have error message if price_cents is empty' do
+      expect(@Price.price_cents).to be_nil
+      expect(@Price.errors.full_messages).to include("Price can't be blank")
     end
 
-    it 'should have all fields but quantity' do
-      expect(@productQuantity.name).to be_present
-      expect(@productQuantity.price_cents).to be_present
-      expect(@productQuantity.quantity).to be_nil
-      expect(@productQuantity.category_id).to be_present
-
-      expect(@productQuantity.errors.full_messages).to include("Quantity can't be blank")
+    it 'should have error message if quantity is empty' do
+      expect(@Quantity.quantity).to be_nil
+      expect(@Quantity.errors.full_messages).to include("Quantity can't be blank")
     end
 
-    it 'should have all fields but category' do
-      expect(@productCategory.name).to be_present
-      expect(@productCategory.price_cents).to be_present
-      expect(@productCategory.quantity).to be_present
-      expect(@productCategory.category_id).to be_nil
-
-      expect(@productCategory.errors.full_messages).to include("Category can't be blank")
+    it 'should have error message if category_id is empty' do
+      expect(@Category.category_id).to be_nil
+      expect(@Category.errors.full_messages).to include("Category can't be blank")
     end
 
   end
